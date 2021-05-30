52b5813fde504f5d4bb7f39c1802ff8dfa1aee6d
Mahmuod1 <79579124+Mahmuod1@users.noreply.github.com>
2021-05-30 14:18:26 +0200

    ..

--- server.js
+++ server.js
@@ -9,18 +9,17 @@ const auth = require('./controllers/auth')
 const server= http.createServer(app)
 
 app.use(cors('*'))
-const io=require('socket.io')(server,{
-   cors: { 'Access-Control-Allow-Credentials': true,
-'Access-Control-Allow-Methods': ['GET', 'PUT', 'POST', 'DELETE', 'HEAD', 'PATCH', 'OPTIONS'],
-'Access-Control-Allow-Origin': 'http://localhost:3000',
-'Connection': 'keep-alive',
-'Content-Length': 1,
-'Content-Type': 'text/plain; charset=UTF-8',
-'Vary': 'Origin'
+const io = require('socket.io')(server,(req, res, next) => {
 
-}       
-    }
-)
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader('Connection','keep-alive')
+res.setHeader('Content-Type','text/plain; charset=UTF-8')
+  res.setHeader("Access-Control-Allow-Origin",'http://localhost:3000')
+  res.setHeader("Access-Control-Allow-Methods", "POST,HEAD,GET,POT,PATCH,DELETE,OPTIONS,PUT");
+  console.log('done headers')
+  req.setHeader("Access-Control-Allow-Origin",'http://localhost:3000')
+  next();
+})
 
 
 const user= require('./controllers/user')
@@ -43,12 +42,12 @@ socket.on('messageToGroup',(msg)=>{
 Message.create(msg).then(data=>{
     
     io.to(msg.to).emit('messageToClient',data)
-    callback();
+    
 }).catch(e=>{
     io.to(msg.to).emit('error',{error:e})
 })
 
-},callback)
+})
 
     })
 app.use('/message',message)
